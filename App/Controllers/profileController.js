afaf219fa4d98061e2349cb9a8c0e4c15092f3b1
Simeon <simeon.simka@gmail.com>
2021-04-22 01:37:40 +0300

    user panel and basic post templates created

--- App/Controllers/profileController.js
+++ App/Controllers/profileController.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     const user = await req.user;
-    res.render('profile', { title: 'Profile', user: user?.username || 'User' });
+    res.render('profile', { title: 'Profile', user: user });
 });
 
 
