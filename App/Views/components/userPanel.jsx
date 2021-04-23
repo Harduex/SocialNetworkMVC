dd8e96d84b807bf11eb4273c048d32953e313f4f
Simeon <simeon.simka@gmail.com>
2021-04-23 16:32:39 +0300

    some customization updates

--- App/Views/components/userPanel.jsx
+++ App/Views/components/userPanel.jsx
@@ -25,28 +25,32 @@ function UserPanel(props) {
                   {/* BEGIN profile-header-info */}
                   <div className="profile-header-info">
                     <h4 className="m-t-10 m-b-5 user-full-name">{props?.user?.fullName || 'user full name'}</h4>
-                    <a href={`user/${props?.user?.username}`} className="m-b-10 text-light user-username">@{props?.user?.username || 'user'}</a>
-
-                    {props.loggedIn ?
-                      <a href="/profile/edit" className="btn btn-sm btn-primary mb-2">Edit Profile</a>
-                      :
-                      <a href={`/user/follow/${props?.user?.username}`} className="btn btn-sm btn-primary mb-2" data-pjax='main'>{props?.follow}</a>
-                    }
-
+                    <a href={`user?username=${props?.user?.username}`} className="m-b-10 text-light user-username">@{props?.user?.username || 'user'}</a>
+                    <p className="m-b-10 user-bio">{props?.user?.bio || ''}</p>
                   </div>
+                  {props.loggedIn ?
+                    <div className="profile-options">
+                      <a href="/profile/edit" className="btn btn-md btn-primary edit-profile-button">
+                        <i class="fa fa-lg fa-user-cog text-white"></i>
+                      </a>
+                    </div>
+                    :
+                    <div className="profile-options">
+                      <a href={`/user/follow/${props?.user?.username}`} className="btn btn-sm btn-primary mb-2">{props?.follow}</a>
+                    </div>
+                  }
                   {/* END profile-header-info */}
                 </div>
                 {/* END profile-header-content */}
 
-                {/* BEGIN profile-header-tab */}
-                <ul className="profile-header-tab nav nav-tabs">
-                  <li className="nav-item"><a href="#posts" className="nav-link text-white" data-toggle="tab">Posts {(props?.posts?.length || 0)}</a></li>
-                  <li className="nav-item"><a href="#followers" className="nav-link text-white" data-toggle="tab">Followers {(props?.user?.followers?.length || 0)}</a></li>
-                  <li className="nav-item"><a href="#following" className="nav-link text-white" data-toggle="tab">Following {(props?.user?.following?.length || 0)}</a></li>
-                </ul>
-                {/* END profile-header-tab */}
-
               </div>
+              {/* BEGIN profile-header-tab */}
+              <ul className="profile-header-tab nav nav-tabs">
+                <li className="nav-item"><a href="#posts" className="nav-link text-white" data-toggle="tab">Posts {(props?.posts?.length || 0)}</a></li>
+                <li className="nav-item"><a href="#followers" className="nav-link text-white" data-toggle="tab">Followers {(props?.user?.followers?.length || 0)}</a></li>
+                <li className="nav-item"><a href="#following" className="nav-link text-white" data-toggle="tab">Following {(props?.user?.following?.length || 0)}</a></li>
+              </ul>
+              {/* END profile-header-tab */}
             </div>
           </div>
         </div>
