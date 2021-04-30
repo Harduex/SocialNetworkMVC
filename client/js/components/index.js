3a4e95723daa0db3364991615a62539e223d83f8
Simeon <simeon.simka@gmail.com>
2021-05-01 00:29:12 +0300

    refresh timeline button added

--- client/js/components/index.js
+++ client/js/components/index.js
@@ -1,5 +1,17 @@
 $(document).ready(function () {
     // Ajax
+    $(document).on('click', '.update-feed-posts', function () {
+        $.ajax({
+            url: "/posts",
+            method: "POST",
+            data: { page: 1 },
+            dataType: "html"
+        })
+            .done(function (data) {
+                $(".posts-container").html(data);
+            });
+    });
+
     let page = 1;
     $(document).on('click', '.load-more-feed-posts', function () {
         page++;
