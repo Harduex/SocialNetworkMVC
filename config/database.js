6b2f3b64be89cd587d05c355fb15ef4564052b16
Simeon <simeon.simka@gmail.com>
2021-05-08 20:08:01 +0300

    Db config updated, added .env config

--- config/database.js
+++ config/database.js
@@ -1,10 +1,18 @@
 import mongoose from 'mongoose';
 
 const dbConnection = () => {
-    // credentials
-    const dbUrl = 'localhost';
-    const dbPort = '27017';
-    const database = 'SocialNetwork';
+    // Manual configuration
+    const dbPrefix = process.env.DB_PREFIX ? `${process.env.DB_PREFIX}` : 'mongodb';
+    const dbUser = process.env.DB_USER ? `${process.env.DB_USER}:` : '';
+    const dbPassword = process.env.DB_PASSWORD ? `${process.env.DB_PASSWORD}@` : '';
+    const dbUrl = process.env.DB_URL || 'localhost';
+    const dbPort = process.env.DB_PORT ? `:${process.env.DB_PORT}` : '';
+    const dbName = process.env.DB_NAME || 'MyDb';
+
+    // Or full db uri connection string if you prefer
+    const fullDbUri = process.env.DB_URI || '';
+
+    const dbUri = fullDbUri || `${dbPrefix}://${dbUser}${dbPassword}${dbUrl}${dbPort}/${dbName}`;
 
     // options
     const options = {
@@ -14,10 +22,11 @@ const dbConnection = () => {
     }
 
     // connection
-    mongoose.connect(`mongodb://${dbUrl}:${dbPort}/${database}`, options);
+    mongoose.connect(dbUri, options);
+
     // if mongoose runs
     mongoose.connection.on('open', () => {
-        console.log('Mongodb Connected');
+        console.log(`Mongodb Connected: ${dbUri}`);
     });
     // error
     mongoose.connection.on('error', (error) => {
