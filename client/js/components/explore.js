5c2164721afae4683fad63df8773620e27db7b05
Harduex <simeon.simka@gmail.com>
2021-10-13 00:19:38 +0300

    explore carousel responsiveness added

--- client/js/components/explore.js
+++ client/js/components/explore.js
@@ -2,7 +2,33 @@ $(document).ready(function () {
     $('.explore-carousel').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 3,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
       });
 });
 
