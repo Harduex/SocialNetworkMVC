e1ca8bacbd4d75638398da6fb04268222f0f5201
Harduex <simeon.simka@gmail.com>
2021-10-31 22:59:09 +0200

    Update uploadImage.js

--- App/helpers/middlewares/uploadImage.js
+++ App/helpers/middlewares/uploadImage.js
@@ -1,10 +1,14 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        const absPath = path.join(__dirname, '../../../', 'public/temp');
-        cb(null, absPath);
+        const tempDir = path.join(__dirname, '../../../public/temp');
+        if (!fs.existsSync(tempDir)) {
+            fs.mkdirSync(tempDir)
+        }
+        cb(null, tempDir);
     },
     filename: function (req, file, cb) {
         const extension = path.extname(file.originalname);
